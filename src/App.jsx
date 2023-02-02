aef38f6a8d078da01ab8b2d64905897eb48fd0c6
dangthanhthanh <dangtanthanh12a2@gmail.com>
2023-02-02 21:03:12 +0700

    addtodo.removetodo

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import ChildOne from './components/ChildOne'
 const initialState={
   number:0,
+  todos:[],
+  addtodo:()=>{},
   post:{
     loading:false,
     data:[],
@@ -26,6 +28,25 @@ function appReducer(state,action){
         ...state,
         number:state.number--
       }
+    case 'add-todo':
+      return{
+        ...state,
+        todos:state.todos.concat(action.payload)
+      }
+    case 'removeTodo':
+      const index=action.payload
+      const clonetodo= [...state.todos]
+      clonetodo.splice(index,1)
+      console.log('lan')
+        return{
+          ...state,
+          todos:clonetodo
+        }
+    // case 'editTodo':
+    //     return{
+    //         ...state,
+    //         todos:state.todos.splice(action.id,1)
+    // }
     case 'post-request':
     case 'post-request-success':
     case 'post-request-error':
@@ -59,11 +80,34 @@ function App() {
       type:'decreace',
     })
   }
+  const addtodo=(fromvalue)=>{
+    dispatch({
+      type:"add-todo",
+      payload:fromvalue,
+    })
+  }
+
+  //add,remove
+  const editTodo=(fromvalue)=>{
+    dispatch({
+      type:"editTodo",
+      payload:fromvalue,
+    })
+  }
+  const removeTodo=(todoIndex)=>{
+    dispatch({
+      type:"removeTodo",
+      payload:todoIndex,
+    })
+  }
   const value={
     imgUrl:url,
     count:reducer.number,
     onIncreace:onIncreace,
     onDecreace:onDecreace,
+    todos:reducer.todos,
+    addtodo,
+    removeTodo,
   }
   return (
     <AppContext.Provider value={value}>
--- src/components/ChildTwo.jsx
+++ src/components/ChildTwo.jsx
@@ -1,8 +1,39 @@
+import { useContext, useState } from "react"
+import { AppContext } from "../App"
 import ChildThree from "./ChildThree"
 
 const ChildTwo=()=>{
+    const {todos,addtodo,removeTodo}=useContext(AppContext)
+    const [fromvalue,setFromvalue]=useState([])
+    const onChangeInput=(e)=>{
+        setFromvalue(e.target.value)
+    }
+    const onAddtodo=()=>{
+        addtodo(fromvalue);
+        setFromvalue('')
+        console.log(todos)
+    }
+    const onEdit=(e)=>{
+        console.log(e)
+
+    }
+    
     return <div>
         <ChildThree />
+        <input onChange={onChangeInput}
+        type="text"
+        value={fromvalue} />
+        <button onClick={onAddtodo}>addtodo</button>
+        <ul>
+            {todos.map((it,index)=>{
+                return(
+                    <li key={index}>{it}
+                    <button onClick={onEdit}>edit</button>
+                    <button onClick={()=>{removeTodo(index)}}>remove</button>
+                    </li>
+                )
+            })}
+        </ul>
     </div>
 }
 export default ChildTwo
