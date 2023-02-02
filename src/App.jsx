e7a95683695b5bb92128223aff3a3f67fd49a553
dangthanhthanh <dangtanthanh12a2@gmail.com>
2023-02-02 19:52:04 +0700

    add reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,69 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
 import './App.css'
 import ChildOne from './components/ChildOne'
+const initialState={
+  number:0,
+  post:{
+    loading:false,
+    data:[],
+    meta:{
+      page:1,
+      nextPage:true,
+      prevPage:false,
+    }
+  }
+}
+// "post-request"=>"post-request-success"=>"post-request-error"
+function appReducer(state,action){
+  switch(action.type){
+    case 'increace':
+      return{
+        ...state,
+        number:state.number++
+      }
+    case 'decreace':
+      return{
+        ...state,
+        number:state.number--
+      }
+    case 'post-request':
+    case 'post-request-success':
+    case 'post-request-error':
+
+      default:
+      break;
+    }
+}
+//call api
+
+
+//usecontext
 export const AppContext=createContext({
-  imgUrl:''
+  // imgUrl:'',
+  // count:reducer.number,
+  // onIncreace:onIncreace,
+  // onDecreace:onDecreace,
+  // ontop error because default funtion
 })
 function App() {
+  const [reducer,dispatch]=useReducer(appReducer,initialState)
   const url='/vite.svg'
+  console.log(reducer);
+  const onIncreace=()=>{
+    dispatch({
+      type:'increace',
+    })
+  }
+  const onDecreace=()=>{
+    dispatch({
+      type:'decreace',
+    })
+  }
   const value={
     imgUrl:url,
+    count:reducer.number,
+    onIncreace:onIncreace,
+    onDecreace:onDecreace,
   }
   return (
     <AppContext.Provider value={value}>
--- src/components/ChildFour.jsx
+++ src/components/ChildFour.jsx
@@ -2,8 +2,11 @@ import { useContext } from "react"
 import { AppContext } from "../App"
 
 const ChildFour=()=>{
-    const{imgUrl}=useContext(AppContext)
-    return <div>
+    const{imgUrl:imageUrl,count,onIncreace,onDecreace}=useContext(AppContext)
+    return <div style={{display:'flex',flexDirection:"column",alignItems:'center',justifyContent:'center'}}>
+        <div>{count}</div>
+        <button onClick={onIncreace}>Increace</button>
+        <button onClick={onDecreace}>Decreace</button>
         <img width={"200px"} src={imageUrl} alt='taolaoapp.com'/>
     </div>
 }
